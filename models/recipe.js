6d6df049672e8882b922327c05b72e81618015ee
IvanGent <gentilleivan@gmail.com>
2020-10-15 19:36:30 -0400

    Phase 11 completed

--- models/recipe.js
+++ models/recipe.js
@@ -1,12 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define('Recipe', {
-    title: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      },
+    },
   }, {});
   Recipe.associate = function(models) {
     // associations can be defined here
-    Recipe.hasMany(models.Instruction, { foreignKey: 'recipeId' });
-    Recipe.hasMany(models.Ingredient, { foreignKey: 'recipeId' });
+    Recipe.hasMany(models.Instruction, { foreignKey: 'recipeId', onDelete: 'CASCADE', hooks: true });
+    Recipe.hasMany(models.Ingredient, { foreignKey: 'recipeId', onDelete: 'CASCADE', hooks: true });
   };
   return Recipe;
 };
